00b01905d4e43c47bb1d4d89a5b423886d0100f6
Shivam-nagar23 <shivam@devtron.ai>
2024-10-24 00:13:01 +0530

    dberr

--- git-sensor/pkg/git/Watcher.go
+++ git-sensor/pkg/git/Watcher.go
@@ -219,7 +219,7 @@ func (impl GitWatcherImpl) pollGitMaterialAndNotify(material *sql.GitMaterial) (
 				if err != nil {
 					dbErr := impl.ciPipelineMaterialRepository.UpdateMaterialsErroredForGitMaterialId(material.Id, sql.SOURCE_TYPE_BRANCH_FIXED)
 					if dbErr != nil {
-						impl.logger.Errorw("error encountered in updating ci pipeline material", "materialId", material.Id, "err", err)
+						impl.logger.Errorw("error encountered in updating ci pipeline material", "materialId", material.Id, "dbErr", dbErr)
 					}
 					impl.logger.Errorw("error in fetching material details in retry", "repo", material.Url, "err", err)
 					return errMsg, err
@@ -228,7 +228,7 @@ func (impl GitWatcherImpl) pollGitMaterialAndNotify(material *sql.GitMaterial) (
 		} else {
 			dbErr := impl.ciPipelineMaterialRepository.UpdateMaterialsErroredForGitMaterialId(material.Id, sql.SOURCE_TYPE_BRANCH_FIXED)
 			if dbErr != nil {
-				impl.logger.Errorw("error encountered in updating ci pipeline material", "materialId", material.Id, "err", err)
+				impl.logger.Errorw("error encountered in updating ci pipeline material", "materialId", material.Id, "dbErr", dbErr)
 			}
 			return errMsg, err
 		}
