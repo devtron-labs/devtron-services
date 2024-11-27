071f27c430bcbdc5f596732a622e662a1ed3b042
Ash-exp <asutosh2000ad@gmail.com>
2024-11-28 04:06:45 +0530

    added unit test cases

--- common-lib/ci-runner/bean/bean.go
+++ common-lib/ci-runner/bean/bean.go
@@ -4,10 +4,10 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"github.com/aws/aws-sdk-go/private/protocol"
 	"os"
 	"path"
 	"strconv"
+	"time"
 )
 
 // ------------
@@ -69,24 +69,51 @@ func (d *Format) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
+func isValidTimeStamp(candidate string, format string) bool {
+	_, err := time.Parse(format, candidate)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
 // isValidDateInput tries to parse the time string with multiple formats\
 // and returns true if the time string is valid
-func isValidDateInput(v string) bool {
-	formats := []string{
-		protocol.RFC822TimeFormatName,
-		protocol.ISO8601TimeFormatName,
-		protocol.UnixTimeFormatName,
+func isValidDateInput(candidate string) bool {
+	timeFormats := [...]string{
+		time.ANSIC,
+		time.UnixDate,
+		time.RubyDate,
+		time.RFC822,
+		time.RFC822Z,
+		time.RFC850,
+		time.RFC1123,
+		time.RFC1123Z,
+		time.Kitchen,
+		time.RFC3339,
+		time.RFC3339Nano,
+		time.DateTime,
+		time.DateOnly,
+		time.Stamp,
+		time.StampMilli,
+		time.StampMicro,
+		time.StampNano,
+		time.TimeOnly,
+		"2006-01-02",                         // RFC 3339
+		"2006-01-02 15:04",                   // RFC 3339 with minutes
+		"2006-01-02 15:04:05",                // RFC 3339 with seconds
+		"2006-01-02 15:04:05-07:00",          // RFC 3339 with seconds and timezone
+		"2006-01-02T15Z0700",                 // ISO8601 with hour; replace Z with either + or -. use Z for UTC
+		"2006-01-02T15:04Z0700",              // ISO8601 with minutes; replace Z with either + or -. use Z for UTC
+		"2006-01-02T15:04:05Z0700",           // ISO8601 with seconds; replace Z with either + or -. use Z for UTC
+		"2006-01-02T15:04:05.999999999Z0700", // ISO8601 with nanoseconds; replace Z with either + or -. use Z for UTC
 	}
-	isValid := false
-	for _, f := range formats {
-		_, err := protocol.ParseTime(f, v)
-		// if err is nil, the time string is valid
-		if err == nil {
-			isValid = true
-			break
+	for _, format := range timeFormats {
+		if isValidTimeStamp(candidate, format) {
+			return true
 		}
 	}
-	return isValid
+	return false
 }
 
 func (d Format) Convert(value string) (interface{}, error) {
