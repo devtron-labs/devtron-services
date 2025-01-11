43788499c055304974edd142107bb2290d7b1984
ayu-devtron <ayush.singh@devtron.ai>
2025-01-11 20:34:01 +0530

    improve pgQueryMonitoringConfig

--- common-lib/utils/bean/bean.go
+++ common-lib/utils/bean/bean.go
@@ -63,13 +63,18 @@ type DockerRegistryInfo struct {
 	DockerRepository   string `json:"dockerRepository"`
 }
 
-type PgQueryConfig struct {
-	LogSlowQuery           bool
-	LogAllQuery            bool
-	LogAllFailureQueries   bool
-	ExportPromMetrics      bool
-	QueryDurationThreshold int64
+type PgQueryMonitoringConfig struct {
+	LogSlowQuery           bool  `env:"PG_LOG_SLOW_QUERY" envDefault:"true"`
+	LogAllQuery            bool  `env:"PG_LOG_ALL_QUERY" envDefault:"false"`
+	LogAllFailureQueries   bool  `env:"PG_LOG_ALL_FAILURE_QUERIES" envDefault:"true"`
+	ExportPromMetrics      bool  `env:"PG_EXPORT_PROM_METRICS" envDefault:"true"`
+	QueryDurationThreshold int64 `env:"PG_QUERY_DUR_THRESHOLD" envDefault:"5000"`
 	ServiceName            string
+}
+
+func (c PgQueryMonitoringConfig) WithServiceName(serviceName string) PgQueryMonitoringConfig {
+	c.ServiceName = serviceName
+	return c
 }
 
 type PgQueryEvent struct {
