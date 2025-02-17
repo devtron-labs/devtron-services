297ed6f674a0055456fa124de50400ebccc9e0d2
Rajeev <rajeev.ranjan@devtron.ai>
2025-02-17 16:29:23 +0530

    added the chart update during templating

--- kubelink/pkg/service/helmApplicationService/helmAppService.go
+++ kubelink/pkg/service/helmApplicationService/helmAppService.go
@@ -1191,9 +1191,27 @@ func (impl *HelmAppServiceImpl) TemplateChart(ctx context.Context, request *clie
 		}
 	case false:
 		chartName = fmt.Sprintf("%s/%s", request.ChartRepository.Name, request.ChartName)
+		chartRepoRequest := request.ChartRepository
+		chartRepoName := chartRepoRequest.Name
+		// Add or update chart repo starts
+		chartRepo := repo.Entry{
+			Name:     chartRepoName,
+			URL:      chartRepoRequest.Url,
+			Username: request.ChartRepository.Username,
+			Password: request.ChartRepository.Password,
+			// Since helm 3.6.1 it is necessary to pass 'PassCredentialsAll = true'.
+			PassCredentialsAll:    true,
+			InsecureSkipTLSverify: chartRepoRequest.GetAllowInsecureConnection(),
+		}
 		username = request.ChartRepository.Username
 		password = request.ChartRepository.Password
 		allowInsecureConnection = request.ChartRepository.AllowInsecureConnection
+		impl.logger.Debug("Adding/Updating Chart repo")
+		err = helmClientObj.AddOrUpdateChartRepo(chartRepo)
+		if err != nil {
+			impl.logger.Errorw("Error in add/update chart repo ", "err", err)
+			return "", nil, err
+		}
 	}
 
 	chartSpec := &helmClient.ChartSpec{
