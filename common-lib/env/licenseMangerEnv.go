16989d759b1ea9deb771354aa6e532e340e8d34f
Shivam-nagar23 <shivam@devtron.ai>
2025-03-17 08:31:10 +0530

    renaming

--- common-lib/env/licenseMangerEnv.go
+++ common-lib/env/licenseMangerEnv.go
@@ -5,13 +5,13 @@ import (
 	"github.com/caarlos0/env"
 )
 
-type ReminderThreshold struct {
+type ReminderThresholdConfig struct {
 	ReminderThresholdForFreeTrial int `env:"REMINDER_THRESHOLD_FOR_FREE_TRIAL" envDefault:"3" description:"this is used to set ( to be remind in time) for free trial"`
 	ReminderThresholdForLicense   int `env:"REMINDER_THRESHOLD_FOR_LICENSE" envDefault:"15" description:"this is used to set ( to be remind in time) for license"`
 }
 
-func GetThresholdRemainder() (*ReminderThreshold, error) {
-	cfg := &ReminderThreshold{}
+func GetThresholdReminderConfig() (*ReminderThresholdConfig, error) {
+	cfg := &ReminderThresholdConfig{}
 	err := env.Parse(cfg)
 	if err != nil {
 		fmt.Println("failed to parse thresholdRemainder for licenses: " + err.Error())
