535959c39564cb96dd6b71ee78020348fbd63947
Shivam-nagar23 <shivam@devtron.ai>
2025-03-22 09:32:36 +0530

    temp commit

--- authenticator/oidc/oidc.go
+++ authenticator/oidc/oidc.go
@@ -136,7 +136,7 @@ func (a *ClientApp) UpdateConfig(c *ClientApp) {
 }
 
 type RedirectUrlSanitiser func(url string) string
-type UserVerifier func(claims jwt.MapClaims) bool
+type UserVerifier func(claims jwt.MapClaims) (bool, error)
 
 func GetScopesOrDefault(scopes []string) []string {
 	if len(scopes) == 0 {
@@ -457,10 +457,11 @@ func (a *ClientApp) HandleCallback(w http.ResponseWriter, r *http.Request) {
 	}
 	returnUrl := appState.ReturnURL
 	// verify user in system
-	valid := a.userVerifier(claims)
+	valid, err := a.userVerifier(claims)
 	//  end verify user in system
 	if !valid {
 		w.Header().Add("Set-Cookie", "")
+		_, err = w.Write([]byte(fmt.Sprintf("Login failed: %v", err)))
 		returnUrl = NoUserLocation
 	} else {
 		if idTokenRAW != "" {
