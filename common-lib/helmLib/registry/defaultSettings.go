9a8d03b5c7644e4c34958ecfec4fcb95cf831b79
ayushmaheshwari <ayush@devtron.ai>
2025-04-15 15:18:59 +0530

    fixing plain http option

--- common-lib/helmLib/registry/defaultSettings.go
+++ common-lib/helmLib/registry/defaultSettings.go
@@ -3,6 +3,7 @@ package registry
 import (
 	"go.uber.org/zap"
 	"helm.sh/helm/v3/pkg/registry"
+	"strings"
 )
 
 type DefaultSettingsGetter interface {
@@ -45,7 +46,8 @@ func (s *DefaultSettingsGetterImpl) getRegistryClient(config *Configuration) (*r
 	}
 
 	clientOptions := []registry.ClientOption{registry.ClientOptHTTPClient(httpClient)}
-	if config.RegistryConnectionType == INSECURE_CONNECTION {
+
+	if strings.Contains(strings.ToLower(config.RegistryUrl), "http") {
 		clientOptions = append(clientOptions, registry.ClientOptPlainHTTP())
 	}
 
