92902b38d413c58ea1b5eaa45e650fc817f8dd92
kartik-579 <kartik@devtron.ai>
2025-05-26 17:14:56 +0530

    added env var category

--- common-lib/utils/grpc/GrpcConfig.go
+++ common-lib/utils/grpc/GrpcConfig.go
@@ -2,6 +2,7 @@ package grpc
 
 import "github.com/caarlos0/env"
 
+// CATEGORY=INFRA_SETUP
 type Configuration struct {
 	KubelinkMaxRecvMsgSize    int    `env:"KUBELINK_GRPC_MAX_RECEIVE_MSG_SIZE" envDefault:"20"` // In mb
 	KubelinkMaxSendMsgSize    int    `env:"KUBELINK_GRPC_MAX_SEND_MSG_SIZE" envDefault:"4"`     // In mb
