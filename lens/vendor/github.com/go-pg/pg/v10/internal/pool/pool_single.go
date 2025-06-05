3c18ccee2f641a4e3ce0102fcaa7386644e1a9e8
prakhar katiyar <39842461+prkhrkat@users.noreply.github.com>
2025-06-05 17:19:55 +0530

    Merge pull request #282 from devtron-labs/dependa-fix
    misc: Dependabot changes to fix vulnerabilities

--- lens/vendor/github.com/go-pg/pg/v10/internal/pool/pool_single.go
+++ lens/vendor/github.com/go-pg/pg/v10/internal/pool/pool_single.go
@@ -37,6 +37,11 @@ func (p *SingleConnPool) Put(ctx context.Context, cn *Conn) {}
 func (p *SingleConnPool) Remove(ctx context.Context, cn *Conn, reason error) {
 	p.cn = nil
 	p.stickyErr = reason
+	// If ctx is cancelled without a reason(error) value,
+	// then the ctx.Error is used as the reason for why the p.cn is assigned nil.
+	if reason == nil && ctx != nil {
+		p.stickyErr = ctx.Err()
+	}
 }
 
 func (p *SingleConnPool) Close() error {
@@ -55,4 +60,20 @@ func (p *SingleConnPool) IdleLen() int {
 
 func (p *SingleConnPool) Stats() *Stats {
 	return &Stats{}
+}
+
+func (p *SingleConnPool) GetWriteBuffer() *WriteBuffer {
+	return p.pool.GetWriteBuffer()
+}
+
+func (p *SingleConnPool) PutWriteBuffer(wb *WriteBuffer) {
+	p.pool.PutWriteBuffer(wb)
+}
+
+func (p *SingleConnPool) GetReaderContext() *ReaderContext {
+	return p.pool.GetReaderContext()
+}
+
+func (p *SingleConnPool) PutReaderContext(rd *ReaderContext) {
+	p.pool.PutReaderContext(rd)
 }
