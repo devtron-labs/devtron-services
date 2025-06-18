0f23fa6e5c4302f25fc8dcc2f2f83e733335923b
Ash-exp <asutosh2000ad@gmail.com>
2025-06-19 05:19:06 +0530

    chore: refactoring for ent

--- ci-runner/helper/CommandExecuter.go
+++ ci-runner/helper/CommandExecuter.go
@@ -24,11 +24,14 @@ func (c *CommandExecutorImpl) RunCommand(ctx cicxt.CiContext, cmd *exec.Cmd) err
 }
 
 func (c *CommandExecutorImpl) RunCommandWithCtx(ctx cicxt.CiContext, cmd *exec.Cmd) error {
+	return util.RunCommand(c.GetCommandWithCtx(ctx, cmd))
+}
+
+func (c *CommandExecutorImpl) GetCommandWithCtx(ctx cicxt.CiContext, cmd *exec.Cmd) *exec.Cmd {
 	// Ensure the command is run with the provided context
 	var args []string
 	if len(cmd.Args) > 1 {
 		args = cmd.Args[1:]
 	}
-	exec.CommandContext(ctx, cmd.Path, args...)
-	return util.RunCommand(cmd)
+	return exec.CommandContext(ctx, cmd.Path, args...)
 }
