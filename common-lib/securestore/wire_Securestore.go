28668abe84a79d69463aa0547783b26c765fb159
kartik-579 <kartik@devtron.ai>
2025-10-07 10:56:39 +0530

    wip

---
+++ common-lib/securestore/wire_Securestore.go
@@ -0,0 +1,9 @@
+package securestore
+
+import "github.com/google/wire"
+
+var SecurestoreWireSet = wire.NewSet(
+	NewAttributesRepositoryImpl,
+	wire.Bind(new(AttributesRepository), new(*AttributesRepositoryImpl)),
+	NewEncryptionKeyServiceImpl,
+	wire.Bind(new(EncryptionKeyService), new(*EncryptionKeyServiceImpl)))
